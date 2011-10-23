81b145b74b5b8a8f07ecd1d62d7f45e89291f100
Jonas Bang Christensen <email@jonasbang.dk>
2011-10-23 18:24:44 +0200

    Hide page title on front page.

--- content-page.php
+++ content-page.php
@@ -10,7 +10,10 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
+        <?php if ( !is_front_page () ) : ?>
+                <h1 class="entry-title"><?php the_title(); ?></h1>
+        <?php endif; ?>
+		
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
