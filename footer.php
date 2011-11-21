180cc427ac38c0b636e0fdebed303c2a4f9f3ff0
Jonas Bang Christensen <email@jonasbang.dk>
2011-11-21 22:12:30 +0100

    Change footer text from Wordpress link to Copyright info.

--- footer.php
+++ footer.php
@@ -22,8 +22,7 @@
 			?>
 
 			<div id="site-generator">
-				<?php do_action( 'twentyeleven_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyeleven' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'twentyeleven' ), 'WordPress' ); ?></a>
+                                &copy; <?php bloginfo('name');?> - All rights reserved
 			</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
